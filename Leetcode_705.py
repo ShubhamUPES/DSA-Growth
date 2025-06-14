e3a67e05915540b7d04fdae2d5ca22a9d703a588
Shubham Sahu <167737503+ShubhamUPES@users.noreply.github.com>
2025-06-14 12:30:47 +0530

    Update Leetcode_705.py

--- Leetcode_705.py
+++ Leetcode_705.py
@@ -16,3 +16,19 @@ class MyHashSet:
             if self.myhashset[i] == key:
                 return True
         return False
+
+###using set
+'''class MyHashSet:
+
+    def __init__(self):
+        self.set = set()
+
+    def add(self, key: int) -> None:
+        self.set.add(key)
+
+    def remove(self, key: int) -> None:
+        self.set.discard(key)  # `discard` won't raise error if key not present
+
+    def contains(self, key: int) -> bool:
+        return key in self.set
+'''

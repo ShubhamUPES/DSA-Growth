6e258248555046d72533efd7b6b8b679b6ac709f
Shubham Sahu <167737503+ShubhamUPES@users.noreply.github.com>
2025-06-16 23:19:32 +0530

    Create Leetcode_225.py

---
+++ Leetcode_225.py
@@ -0,0 +1,17 @@
+class MyStack:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x):
+        self.queue.append(x)
+        for i in range(len(self.queue) - 1):
+            self.queue.append(self.queue.pop(0))
+
+    def pop(self):
+        return self.queue.pop(0)
+
+    def top(self):
+        return self.queue[0]
+
+    def empty(self):
+        return len(self.queue) == 0

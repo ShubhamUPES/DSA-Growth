6669612125dd7803f468f6e9c83c98b146f17905
Shubham Sahu <167737503+ShubhamUPES@users.noreply.github.com>
2025-07-24 22:46:10 +0530

    Create Leetcode_274.java

---
+++ Leetcode_274.java
@@ -0,0 +1,17 @@
+
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations); // Sort in ascending order
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i; // Number of papers with >= citations[i] citations
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+}

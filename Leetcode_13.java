3013e0985707b6b0117e7986f80faa9b887ebf40
Shubham Sahu <167737503+ShubhamUPES@users.noreply.github.com>
2025-07-25 23:42:07 +0530

    Create Leetcode_13.java

---
+++ Leetcode_13.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> roman = Map.of(
+            'I', 1, 'V', 5, 'X', 10,
+            'L', 50, 'C', 100, 'D', 500, 'M', 1000
+        );
+        
+        int total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int val = roman.get(s.charAt(i));
+            if (i < s.length() - 1 && val < roman.get(s.charAt(i + 1))) {
+                total -= val;
+            } else {
+                total += val;
+            }
+        }
+        return total;
+    }
+}
